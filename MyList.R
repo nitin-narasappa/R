e504b0a5f8eaceb2f40402582d82139678416e7b
Nitin Narasappa <nitingn@gmail.com>
2016-04-02 13:39:27 +1100

    Update MyList.R

--- MyList.R
+++ MyList.R
@@ -1,15 +1,26 @@
 
 # Create a list of movies
-movies <- list("Titanic", "HarryPotter", "Spiderman")
+movies <- list(c(name="Titanic", heroin="Kate", hero="Leonardo"), c(name="HarryPotter"), myfavorate="Spiderman")
 
-# NOTE: How the lists are printed (different to vectors)
+# print the list
 > movies
 [[1]]
-[1] "Titanic"
+      name     heroin       hero 
+ "Titanic"     "Kate" "Leonardo" 
 
 [[2]]
-[1] "HarryPotter"
+         name 
+"HarryPotter" 
 
 [[3]]
+      myfavorate
 [1] "Spiderman"
 
+# Accessing first element
+> movies[1]
+[[1]]
+      name     heroin       hero 
+ "Titanic"     "Kate" "Leonardo"
+ 
+ > movies[3]$myfavorate
+[1] "Spiderman"
