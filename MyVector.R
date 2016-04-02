6c3cfb30a0f070b9600243eb1e2463e4eb05d48c
Nitin Narasappa <nitingn@gmail.com>
2016-04-02 13:13:46 +1100

    Create MyVector.R
    Vector and its basic operations

---
+++ MyVector.R
@@ -0,0 +1,24 @@
+
+# Examples of Vector
+# names, ids, subjects and marks are vectors of different type.
+
+names <- c("John", "Tom", "Bob")
+
+ids <- 5:7
+
+subjects <- c("English", "Maths", "Science")
+
+marks <- c(80, 75, 85)
+
+# Following will print "John" <- first element of vector names.
+print(names[1])
+
+# Mean of the marks vector
+mean(marks)
+
+# Square root of every element of marks
+sqrt(marks)
+
+# multiply every element by 2 and assign it to new vector
+## NOTE:  The other way of assignment.
+marks * 2 -> newMarks
