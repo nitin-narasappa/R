99df611f85e0e823da2dc26d1457e9efd33a1349
Nitin Narasappa <nitingn@gmail.com>
2016-04-02 13:24:36 +1100

    Create MyList.R
    Example of list in R and some basic functions.

---
+++ MyList.R
@@ -0,0 +1,15 @@
+
+# Create a list of movies
+movies <- list("Titanic", "HarryPotter", "Spiderman")
+
+# NOTE: How the lists are printed (different to vectors)
+> movies
+[[1]]
+[1] "Titanic"
+
+[[2]]
+[1] "HarryPotter"
+
+[[3]]
+[1] "Spiderman"
+
